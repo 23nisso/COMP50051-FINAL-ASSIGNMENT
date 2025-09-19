297482bc45640d6844e860c0d40570eb42832c5d
AnisLayaida <nissolay@hotmail.com>
2025-09-19 18:24:50 +0100

    Fix 1

--- src/data-source.ts
+++ src/data-source.ts
@@ -17,7 +17,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    synchronize: process.env.NODE_ENV !== "development",
+    synchronize: process.env.NODE_ENV === "development",
     logging: false,
     entities: [Department, LeaveRequest, LeaveType, Role, User, UserManagement]
 });
